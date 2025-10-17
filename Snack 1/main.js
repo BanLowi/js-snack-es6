7b07bc171d1fad9a981a23c42e1be2cec3a690a1
BanLowi <lowi40k@gmail.com>
2025-10-17 16:58:50 +0200

    format js file

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -1,57 +1,46 @@
 const bikes = [
 
     {
-        name : "AeroBlade SLR",
-        weigth : 7.1
+        name: "AeroBlade SLR",
+        weigth: 7.1
     },
 
     {
-        name : "TrailHunter X9",
-        weigth : 13.4
+        name: "TrailHunter X9",
+        weigth: 13.4
     },
 
     {
-        name : "UrbanRide500",
-        weigth : 11.2
+        name: "UrbanRide500",
+        weigth: 11.2
     },
 
     {
-        name : "GravelOne Pro",
-        weigth : 9.6
+        name: "GravelOne Pro",
+        weigth: 9.6
     },
 
     {
-        name : "ClimbMaster 29",
-        weigth : 12.8
+        name: "ClimbMaster 29",
+        weigth: 12.8
     },
 
     {
-        name : "SpeedWing RS",
-        weigth : 6.9
+        name: "SpeedWing RS",
+        weigth: 6.9
     },
 
     {
-        name : "TourCity Comfort",
-        weigth : 14.3
+        name: "TourCity Comfort",
+        weigth: 14.3
     },
 
     {
-        name : "EnduroMax 800",
-        weigth : 15.5
+        name: "EnduroMax 800",
+        weigth: 15.5
     }
 
 ];
-
-let lightestBike = bikes[0];
-
-
-for (let i = 0; i < bikes.length; i++) {
-    
-    if (bikes[i].weigth < lightestBike.weigth ) {
-        lightestBike = bikes[i]
-    }
-        
-}
 
 function weightCalculator(value) {
 
@@ -59,12 +48,14 @@ function weightCalculator(value) {
 
 
     for (let i = 0; i < value.length; i++) {
-        
-        if (value[i].weigth < lightestBike.weigth ) {
+
+        if (value[i].weigth < lightestBike.weigth) {
             lightestBike = value[i]
         }
-            
+
     }
 
-    return lightestBike
+    return `${lightestBike.name} ${lightestBike.weigth}`
 }
+
+document.getElementById("result").innerHTML = weightCalculator(bikes)
